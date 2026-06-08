0d25c8cb75e430f85e5d17c871d97566d62ffd8f
Vignesh29kumar <vignesh29ramesh@gmail.com>
2026-06-08 15:58:35 +0530

    forntend super

--- frontend-admin/src/services/api.js
+++ frontend-admin/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+// const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://flag-management-system.onrender.com/api";
+
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
