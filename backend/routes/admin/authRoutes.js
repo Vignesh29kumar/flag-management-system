e56bc5c95ca0ebcda5d2c300ce04cdfc95e861db
Vignesh29kumar <vignesh29ramesh@gmail.com>
2026-06-09 18:41:19 +0530

    new change

--- backend/routes/admin/authRoutes.js
+++ backend/routes/admin/authRoutes.js
@@ -2,11 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const { signup, login } = require("../../controllers/admin/authController");
-const { isSuperAdmin } = require("../../middlewares/authMiddleware");
 const { validateAdminSignup, validateAdminLogin } = require("../../validations/adminValidations");
 
 // POST /api/admin/signup
-router.post("/signup", isSuperAdmin, validateAdminSignup, signup);
+router.post("/signup", validateAdminSignup, signup);
 
 // POST /api/admin/login
 router.post("/login", validateAdminLogin, login);
